92225e2a58aaaa09187b12812416eb448ca55f51
stevenjchang <stevenjchang@gmail.com>
2017-08-27 23:53:16 -0700

    comment out (for testing to run without extra logs)

--- src/chapter1/ch1-q9.js
+++ src/chapter1/ch1-q9.js
@@ -13,7 +13,7 @@
  * @param  {string}  str2   String to check if it is a rotated version of str1
  * @return {boolean}        True if str1 and str2 are rotated versions of eachother, otherwise false
  */
-export function isRotatedSubstring(str1, str2) {
+export function _isRotatedSubstring(str1, str2) {
   if (!str1 || !str2) {
     throw new Error('invalid input');
   }
@@ -28,3 +28,24 @@ export function isRotatedSubstring(str1, str2) {
 function isSubstring(str, substr) {
   return str.includes(substr);
 }
+
+
+export function isRotatedSubstring(str1, str2) {
+  if (!str1 || !str2) {
+    throw new Error('invalid input');
+  }
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] === str2[0] && str1[i] !== ' ') {
+      return str2 === rotatedString(str1, i);
+    }
+  }
+  return false;
+}
+// I did it this way, forgot about the trick of doubling str one (str1 + str1)
+// also didn't know about the str.includes()
+let rotatedString = (str, i) => {
+  return str.substring(i) + str.substring(0, i);
+};
